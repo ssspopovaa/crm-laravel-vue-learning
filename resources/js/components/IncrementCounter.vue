3dbca5539eebe1d32cb50ad92cba02c1a37b30b3
Anatoliy Popov <anatoliy@deovr.com>
2025-01-02 23:52:11 +0200

    Add test config vite & vue 3

---
+++ resources/js/components/IncrementCounter.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { ref } from 'vue'
+    const counter = ref(0)
+</script>
+
+<template>
+    <button
+        type="button"
+        @click="counter++"
+        class="p-2 text-white bg-gray-500 rounded"
+    >
+        Counter is: {{ counter }}
+    </button>
+</template>
+
+<style scoped>
+
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel 11 with Vue</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
+
+
+{{--
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -174,3 +189,4 @@
         </div>
     </body>
 </html>
+--}}
